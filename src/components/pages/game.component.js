ecf3278eb5c19f83435e0cb94c76314fd1bbff46
rudenate3 <nathan.odin@gmail.com>
2018-10-02 05:57:50 -0400

    Update to use new game state provided
    -(refactor) Refactor game logic to use new game state provided by api

--- src/components/pages/game.component.js
+++ src/components/pages/game.component.js
@@ -2,69 +2,38 @@
 
 angular.module('hangman').component('game', {
   templateUrl: '/src/components/pages/templates/game.tmpl.html',
-  controller: function(Game) {
+  controller: function(Game, config) {
     const createGameState = game => {
       this.gameState = {}
       if (game.data.game.guesses) {
         game.data.game.incorrectGuesses = game.data.game.guesses.filter(
           guess => {
-            return this.correctLetters.indexOf(guess) < 0
+            return game.data.game.correctGuesses.indexOf(guess) < 0
           }
         )
-        game.data.game.correctGuesses = game.data.game.guesses.filter(guess => {
-          return this.correctLetters.indexOf(guess) > -1
-        })
       }
-      this.correctFields = game.data.game.word
-        .split('')
-        .reduce((prev, curr) => {
-          if (!game.data.game.correctGuesses) {
-            prev.push('-')
-          } else if (game.data.game.correctGuesses.includes(curr)) {
-            prev.push(curr)
-          } else {
-            prev.push('-')
-          }
-          return prev
-        }, [])
+
       this.gameState = game.data.game
       this.guessInput = ''
-      this.imageUrl = `../../../assets/images/${
+      this.imageUrl = `${config.imagePath}/${
         game.data.game.incorrectGuesses
           ? game.data.game.incorrectGuesses.length
           : 0
       }.png`
     }
+
     this.$onInit = () => {
-      Game.playGame().then(game => {
-        this.fields = game.data.game.word.split('')
-        this.correctLetters = Object.keys(
-          game.data.game.word.split('').reduce((prev, curr) => {
-            prev[curr] = true
-            return prev
-          }, {})
-        )
-        createGameState(game)
-      })
+      this.newGame()
     }
 
     this.newGame = () => {
-      Game.playGame().then(game => {
-        this.fields = game.data.game.word.split('')
-        this.correctLetters = Object.keys(
-          game.data.game.word.split('').reduce((prev, curr) => {
-            prev[curr] = true
-            return prev
-          }, {})
-        )
-        createGameState(game)
-      })
+      Game.playGame().then(game => createGameState(game))
     }
 
     this.submitInput = guessInput => {
-      Game.attemptMove(this.gameState.id, guessInput).then(game =>
-        createGameState(game)
-      )
+      Game.attemptMove(this.gameState.id, guessInput).then(game => {
+        if (game.data.game) createGameState(game)
+      })
     }
   }
 })
